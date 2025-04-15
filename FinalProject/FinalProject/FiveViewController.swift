d69e040d15fb80e59627ba89e6202b55738df2c2
Katie Wendt <katielwendt@icloud.com>
2025-04-15 12:11:10 -0500

    instrument sounds added

--- FinalProject/FinalProject/FiveViewController.swift
+++ FinalProject/FinalProject/FiveViewController.swift
@@ -6,9 +6,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class FiveViewController: UIViewController {
 
+//create audioPlayer
+    var audioPlayer: AVAudioPlayer?
+    
+    func playSound(for instrument: String){
+        let filename = "\(instrument)_Sound"
+        
+        guard let url = Bundle.main.url(forResource: filename, withExtension: "wav") else {
+             let url = Bundle.main.url(forResource: filename, withExtension: "mp3")!
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.prepareToPlay()
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,26 +37,32 @@ class FiveViewController: UIViewController {
     
     @IBAction func harpBtn(_ sender: UIButton) {
         //when clicked plays harp sound
+        playSound(for: "harp")
     }
     
     @IBAction func guitarBtn(_ sender: UIButton) {
         //when clicked plays guitar sound
+        playSound(for: "guitar")
     }
     
     @IBAction func violinBtn(_ sender: UIButton) {
         //when clicked plays violin sound
+        playSound(for: "violin")
     }
     
     @IBAction func drumsBtn(_ sender: UIButton) {
         //when clicked plays drums sound
+        playSound(for: "drum")
     }
     
     @IBAction func pianoBtn(_ sender: UIButton) {
         //when clicked plays piano sound
+        playSound(for: "piano")
     }
     
     @IBAction func fluteBtn(_ sender: UIButton) {
         //when clicked plays flute sound
+        playSound(for: "flute")
     }
     
     /*
