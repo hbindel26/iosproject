c6c5ac706794cc1841655117f1614111d977151e
Hayden Bindel <124805187+hbindel26@users.noreply.github.com>
2025-04-15 12:14:54 -0500

    view4controller

--- FinalProject/FinalProject/FourViewController.swift
+++ FinalProject/FinalProject/FourViewController.swift
@@ -6,13 +6,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class FourViewController: UIViewController {
+var audioPlayer: AVAudioPlayer?
 
-    @IBOutlet weak var soundOne: UIButton!
+    func playSound(for cartoon: String){
+        let filename = "\(cartoon)_Sound"
+        guard let url = Bundle.main.url(forResource:filename, withExtension: "mp3") else {
+            print("Could not find sound file: \(filename).mp3")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.prepareToPlay()
+            audioPlayer?.play()
+        }catch {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func carhornSound(_ sender: Any) {
+    }
+    @IBAction func dundunSound(_ sender: Any) {
+    }
+    @IBAction func prowlerSound(_ sender: Any) {
+    }
+    @IBAction func snoreSound(_ sender: Any) {
+    }
+    
+    @IBAction func spongebobSound(_ sender: Any) {
+    }
+    @IBAction func wowSound(_ sender: Any) {
+    }
+    
 }
