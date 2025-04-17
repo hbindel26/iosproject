b5d91d8e212ff36042692ed49eb16fd7fc91cf3f
Katie Wendt <katielwendt@icloud.com>
2025-04-17 11:55:32 -0500

    copied project

---
+++ FinalProject copy/FinalProject/OneViewController.swift
@@ -0,0 +1,74 @@
+//
+//  OneViewController.swift
+//  
+//
+//  Created by Katie Wendt on 4/2/25.
+//
+
+import UIKit
+import AVFoundation
+
+
+class OneViewController: UIViewController {
+var audioPlayer: AVAudioPlayer?
+
+
+    
+    func playSound(for animal: String) {
+        let filename = "\(animal)_Sound"
+        
+        guard let url = Bundle.main.url(forResource: filename, withExtension: "wav") else {
+            print("Could not find sound file: \(filename).wav")
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.prepareToPlay()
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func elephantButton(_ sender: UIButton) {
+        playSound(for: "Elephant")
+        
+    }
+    
+    
+    @IBAction func dogButton(_ sender: UIButton) {
+        playSound(for: "Dog")
+    }
+    
+    @IBAction func hyenaButton(_ sender: Any) {
+        playSound(for: "Hyena")
+    }
+    
+    
+    @IBAction func catButton(_ sender: UIButton) {
+        playSound(for: "Cat")
+    }
+    
+    
+    @IBAction func monkeyButton(_ sender: UIButton) {
+        playSound(for: "Monkey")
+    }
+    
+    
+    @IBAction func donkeyButton(_ sender: UIButton) {
+        playSound(for: "Donkey")
+    }
+}
+
+
+
